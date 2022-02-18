8948925e7d57535265f2518bc1f758fe7c7a0156
YetiYetYet <alexisbehier22@gmail.com>
2022-02-18 21:37:15 +0100

    First Init

---
+++ ConfigurationExtension/ConfigurationExtension.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace ConfiguationExtension;
+
+public static class ConfigurationExtension
+{
+    /// <summary>
+    /// Shorthand for GetSection("ConnectionStrings")[name].
+    /// Throw an exception if the connection sting returned is NullOrEmpty.
+    /// </summary>
+    /// <param name="configuration">The configuration to enumerate.</param>
+    /// <param name="name">The connection string key.</param>
+    /// <exception cref="ConfigurationMissingException">
+    /// Throw an exception if the connection sting returned is NullOrEmpty.</exception>
+    /// <returns>The connection string.</returns>
+    public static string GetConnectionStringOrThrowIfNullOrEmpty(this IConfiguration configuration, string name)
+    {
+        if(!configuration.GetSection("ConnectionStrings").Exists())
+            throw new ConfigurationMissingException(
+                $"There is no section \"ConnectionStrings\") on your configuration.");
+        var connectionString = configuration.GetSection("ConnectionStrings")[name];
+        if (string.IsNullOrEmpty(connectionString))
+            throw new ConfigurationMissingException(
+                $"No connection string defined for {name} on section ConnectionsStrings.");
+        return connectionString;
+    }
+}
---
+++ ConfigurationExtension/ConfigurationMissingExeption.cs
@@ -0,0 +1,14 @@
+﻿namespace ConfiguationExtension;
+
+/// <summary>
+/// Execption throwed when the configuration have a null or empty element.
+/// </summary>
+[Serializable]
+public class ConfigurationMissingException : Exception
+{
+    public ConfigurationMissingException() { }
+    
+    public ConfigurationMissingException(string message) : base(message)  { }
+    
+    public ConfigurationMissingException(string message,Exception inner) : base(message, inner)  { }
+}
